26b8a445ce6b8fecaff1782993637af2e300ad2f
maaz-shahid99 <goluthakle.com>
2023-06-28 03:53:57 +0530

    Login form created

--- CRM/website/views.py
+++ CRM/website/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 # Create your views here.
 def home(request):
+    #Checking for login 
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        #Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged In!")
+            return redirect('home')
+        
+        else:
+            messages.success(request, "There Was An Error Logging In, Please Try Again.")
     return render(request, 'home.html', {})
+
+# def login_user(request):
+    # #Checking for login 
+    # if request.method == 'POST':
+    #     username = request.POST['username']
+    #     password = request.POST['password']
+
+    #     #Authenticate
+    #     user = authenticate(request, username=username, password=password)
+    #     if user is not None:
+    #         login(request, user)
+    #         messages.success(request, "You have been logged In!")
+    #         return redirect('home')
+        
+    #     else:
+    #         messages.success(request, "There Was An Error Logging In, Please Try Again.")
+# def logout_user(request):
+#     pass
